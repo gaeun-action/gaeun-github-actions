4a16ec1ac050587f0d9b9009b545c92880ce139b
kim gaeun <rkdms7220@naver.com>
2023-12-31 20:41:59 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Githubactions 6
+          Learn Githubactions 7
         </a>
       </header>
     </div>
